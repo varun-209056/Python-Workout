75f570caea4445a41542fd176a14fc5a97ce92cf
varun-209056 <71254054+varun-209056@users.noreply.github.com>
2021-12-13 12:15:20 +0530

    Add files via upload

---
+++ Alpha.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+c = int(input("Enter the value of c:"))
+
+d = ((b**2)+(4*a*c - 2)**0.5)/(2*a)
+print(f"The solution of the Equation is:{d}")
