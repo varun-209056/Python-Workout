3dc9a8c9d882239c5b3b47e9df73af84780f45c8
varun-209056 <71254054+varun-209056@users.noreply.github.com>
2022-09-16 12:54:17 +0530

    Add files via upload

---
+++ Zodiac Signs.py
@@ -0,0 +1,87 @@
+print("Enter Your Date of Birth: ")
+m = input(" Month: ")
+d = int(input("Day: "))
+
+if m == "jan" or m == "Jan":
+    if d <= 20:
+        print("Your Zodiac Sign is: Capricorn")
+    elif d>=20:
+        print("Your Zodiac Sign is: Aquarius")
+
+if m == "feb" or m == "Feb":
+    if  d <= 18:
+        print("Your Zodiac Sign is: Aquarius")
+    elif d >= 19:
+        print("Your Zodiac Sign is: Pisces")
+
+if m == "mar" or m == "Mar":
+    if d <= 21:
+        print("Your Zodiac Sign is: Pisces")
+    elif d >= 21:
+        print("Your Zodiac Sign is: Aries")
+
+if m == "apr" or m == "Apr":
+    if d <= 19:
+        print("Your Zodiac Sign is: Aries")
+    elif d >= 20:
+        print("Your Zodiac Sign is: Taurus")
+
+if m == "may" or m == "May":
+    if d <= 20:
+        print("Your Zodiac Sign is: Taurus")
+    elif d >= 21:
+        print("Your Zodiac Sign is: Gemini")
+
+if m == "jun" or m == "Jun":
+    if d <= 20:
+        print("Your Zodiac Sign is: Gemini")
+    elif d >= 21:
+        print("Your Zodiac Sign is: Cancer")
+
+if m == "jul" or m == "Jul":
+    if d <= 22:
+        print("Your Zodiac Sign is: Cancer")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Leo")
+
+if m == "aug" or m == "Aug":
+    if d <= 22:
+        print("Your Zodiac Sign is: Leo")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Virgo")
+
+if m == "sept" or m == "Sept":
+    if d <= 22:
+        print("Your Zodiac Sign is: Virgo")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Libra")
+
+if m == "oct" or m == "Oct":
+    if d <= 22:
+        print("Your Zodiac Sign is: Libra")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Scorpio")
+
+if m == "nov" or m == "Nov":
+    if d <= 22:
+        print("Your Zodiac Sign is: Sagittarius")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Capricorn")
+
+if m == "dec" or m == "Dec":
+    if d <= 22:
+        print("Your Zodiac Sign is: Sagittarius")
+    elif d >= 23:
+        print("Your Zodiac Sign is: Capricorn")
+
+
+
+
+
+
+
+
+        
+    
+    
+    
